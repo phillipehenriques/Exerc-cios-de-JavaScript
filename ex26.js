9b338f4e01d6ee02329ad8813df48fa646b77175
phillipe henriques <phillipe_ch@hotmail.com>
2022-09-05 18:07:45 -0300

    ex26 e ex27

---
+++ ex26.js
@@ -0,0 +1,37 @@
+function  aleatorio(min = 0, max = 3) {
+    min *= 1000
+    max *= 1000
+    return Math.floor(Math.random() * (max - min) + min)
+}
+
+function esperaAi(msg, tempo) {
+    return new Promise((resolve, reject) => {
+    setTimeout(() => {
+        if(typeof msg !== 'string') {
+            reject('CAI NO ERRO')
+            return
+        }
+
+        resolve(msg.toUpperCase() + '- Passei na promise')
+        return
+    }, tempo)    
+    })
+}
+
+esperaAi('Fase 1', aleatorio())
+    .then(valor => {
+        console.log(valor)
+        return esperaAi('Fase 2', aleatorio())
+    })
+    .then(fase => {
+        console.log(fase)
+        return esperaAi('Fase 3', aleatorio())
+    })
+    .then(fase => {
+        console.log(fase)
+        return fase
+    })
+    .then(fase => {
+        console.log('Terminamos na fase', fase)
+    })
+    .catch(e => console.log(e))
---
+++ ex27(Async e Await).js
@@ -0,0 +1,38 @@
+function  aleatorio(min = 0, max = 3) {
+    min *= 1000
+    max *= 1000
+    return Math.floor(Math.random() * (max - min) + min)
+}
+
+function esperaAi(msg, tempo) {
+    return new Promise((resolve, reject) => {
+    setTimeout(() => {
+        if(typeof msg !== 'string') {
+            reject('CAI NO ERRO')
+            return
+        }
+
+        resolve(msg.toUpperCase() + '- Passei na promise')
+        return
+    }, tempo)    
+    })
+}
+
+async function executa() {
+    try {
+        const fase1 = await esperaAi('Fase1', aleatorio())
+    console.log(fase1)
+
+    const fase2 = await esperaAi('Fase2', aleatorio())
+    console.log(fase2)
+
+    const fase3 = await esperaAi('Fase3', aleatorio())
+    console.log(fase3)
+
+    console.log('Teminamos na fase:', fase3)
+    } catch(error) {
+        console.log(error)
+    }
+}
+
+executa()
