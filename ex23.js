e005f248a22d5d78b143c7cc9b0fc50bd998dc8e
phillipe henriques <phillipe_ch@hotmail.com>
2022-09-05 14:38:04 -0300

    correção no ex 23 e 24

--- ex23.js
+++ ex23.js
@@ -1,3 +1,5 @@
 function  aleatorio(min, max) {
+    min *= 1000
+    max *= 1000
     return Math.floor(Math.random() * (max - min) + min)
 }
--- ex24.js
+++ ex24.js
@@ -1,13 +1,18 @@
 function  aleatorio(min, max) {
+    min *= 1000
+    max *= 1000
     return Math.floor(Math.random() * (max - min) + min)
 }
 
 function esperaAi(msg, tempo) {
+    return new Promise((resolve, reject) => {
     setTimeout(() => {
         console.log(msg)
-    }, tempo)
+        resolve()
+    }, tempo)    
+    })
 }
 
-esperaAi('Frase 1', 1000)
-esperaAi('Frase 1', 1000)
-esperaAi('Frase 1', 1000)
+esperaAi('Frase 1', aleatorio(1, 3))
+esperaAi('Frase 2', aleatorio(1, 3))
+esperaAi('Frase 3', aleatorio(1, 3))
