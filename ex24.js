b5013ab4e81387b1ed6a7e4cfab15c48bb19ca39
phillipe henriques <phillipe_ch@hotmail.com>
2022-09-05 14:17:30 -0300

    ex23 e ex24

---
+++ ex24.js
@@ -0,0 +1,13 @@
+function  aleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min) + min)
+}
+
+function esperaAi(msg, tempo) {
+    setTimeout(() => {
+        console.log(msg)
+    }, tempo)
+}
+
+esperaAi('Frase 1', 1000)
+esperaAi('Frase 1', 1000)
+esperaAi('Frase 1', 1000)
